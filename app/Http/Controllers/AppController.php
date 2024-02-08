4b755f88301e457a64b92684d062f028454da64a
developerjame <119099522+developerjame@users.noreply.github.com>
2024-02-08 12:16:48 +0300

    commit

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\FlareClient\View;
 
 class AppController extends Controller
 {
     //
+    public function index(){
+        return view('index');
+    }
 }

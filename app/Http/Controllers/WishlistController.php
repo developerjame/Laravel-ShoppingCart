41c50ce470a09ee84014d0f8f807486ea551860e
developerjame <119099522+developerjame@users.noreply.github.com>
2024-02-14 16:41:59 +0300

    commit

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -7,6 +7,10 @@ use Cart;
 
 class WishlistController extends Controller
 {
+    public function getWishlistedProducts(){
+        $items = Cart::instance("wishlist")->content();
+        return view('wishlist',['items'=>$items]);
+    }
     public function addProductToWishlist(Request $request){
         Cart::instance("wishlist")->add($request->id,$request->name,1,$request->price)->associate('App\Models\Product');
         return response()->json(['status'=> 200,'message'=>'Success ! item successfully added to your wishlist.']);

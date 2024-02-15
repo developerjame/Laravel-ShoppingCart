c392c1fab8afb51f92897a66ca6353af8b1b3b2f
developerjame <119099522+developerjame@users.noreply.github.com>
2024-02-15 13:21:06 +0300

    commit

--- app/Models/Shipping.php
+++ app/Models/Shipping.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Shipping extends Model
 {
     use HasFactory;
+
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
 }
--- database/migrations/2024_02_15_085148_create_shippings_table.php
+++ database/migrations/2024_02_15_085148_create_shippings_table.php
@@ -13,7 +13,19 @@ return new class extends Migration
     {
         Schema::create('shippings', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('order_id')->unsigned();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('locality');
+            $table->text('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('landmark')->nullable();
+            $table->string('zip');
+            $table->string('type')->default('home');
             $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
         });
     }
 

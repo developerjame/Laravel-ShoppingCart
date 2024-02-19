375d7a2392d03517cae3f9feaefd92d63999b4eb
developerjame <119099522+developerjame@users.noreply.github.com>
2024-02-19 23:34:13 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::delete('/cart/clear',[CartController::class,'clearCart'])->name('cart.cle
 
 Route::get('/wishlist',[WishlistController::class,'getWishlistedProducts'])->name('wishlist.list');
 Route::post('/wishlist/add',[WishlistController::class,'addProductToWishlist'])->name('wishlist.store');
-Route::delete('/wishlist/remove',[WishlistController::class,'removeProductFromWishlist'])->name('wishlist.');
+Route::delete('/wishlist/remove',[WishlistController::class,'removeProductFromWishlist'])->name('wishlist.remove');
 Route::delete('/wishlist/clear',[WishlistController::class,'clearWishlist'])->name('wishlist.');
 Route::post('/wishlist/move-to-cart',[WishlistController::class,'moveToCart'])->name('wishlist.move.to.cart');
 
